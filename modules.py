845993f964248009f6a07006c8006382133c9605
Ben Kunkle <ben.kunkle@gmail.com>
2022-01-16 14:57:19 -0800

    modules.py docstrings

--- modules.py
+++ modules.py
@@ -2,15 +2,13 @@ import core
 from pprint import pprint
 
 def get_modules(course_id=72755):
+    """gets modules from course with course_id"""
+
     mods = core.get_request(f"courses/{course_id}/modules")
     modules = {}
     for mod in mods.json():
         name = mod["name"]
         items = core.get_request(mod["items_url"])
         modules[name] = items.json()
-    pprint(modules)
 
-        
-
-
-get_modules()
+    return modules
